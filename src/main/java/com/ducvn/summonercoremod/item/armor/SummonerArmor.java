c2d4034844c5fee708b9df386f86b7d63deb44a8
DUCVN <nguyenvietduc24@yahoo.com.vn>
2024-09-27 21:22:11 +0700

    1.5 update

---
+++ src/main/java/com/ducvn/summonercoremod/item/armor/SummonerArmor.java
@@ -0,0 +1,28 @@
+package com.ducvn.summonercoremod.item.armor;
+
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class SummonerArmor extends ArmorItem {
+    public SummonerArmor(IArmorMaterial p_i48534_1_, EquipmentSlotType p_i48534_2_, Properties p_i48534_3_) {
+        super(p_i48534_1_, p_i48534_2_, p_i48534_3_);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable World world, List<ITextComponent> tooltip, ITooltipFlag flag) {
+        super.appendHoverText(stack, world, tooltip, flag);
+        tooltip.add(new TranslationTextComponent(
+                        "\u00A7b-Full set bonus-\u00A7r"
+                )
+        );
+    }
+}
